5c2e1509ec7886640f21da4b87f4dc882df5534e
Renato Aires <rentonaires@gmail.com>
2023-08-23 17:10:45 -0300

    adicionando header e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>app jsx</h1>
+      <Header />
+      <Footer />
     </>
   )
 }
