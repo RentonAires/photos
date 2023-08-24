0ce3297bc298a2057c13f1788fa4830f145e549f
Renato Aires <rentonaires@gmail.com>
2023-08-23 23:51:04 -0300

    ajustando o header eo footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import Footer from './components/footer/Footer'
-import Header from './components/header/Header'
+import Footer from "./components/Footer/footer"
+import Header from "./components/Header/header"
+
 
 function App() {
   return (
