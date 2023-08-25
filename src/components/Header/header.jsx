7463848de2a487f66beb883d7ed9108a67d8f3ec
Renato Aires <rentonaires@gmail.com>
2023-08-24 23:31:21 -0300

    imagem da logo footer eader

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./header.css";
+import logo from '../../assets/photography-icon.png'
 
 export default function Header(){
     return(
         <>
+            
             <div className="header">
+                <img src={logo} alt="imagem da logo" />
                 <h1>header!!!</h1>
             </div>
         </>
